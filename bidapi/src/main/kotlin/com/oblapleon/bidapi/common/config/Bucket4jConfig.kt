ee114de55011c7bf5985940ad60e41bb3edc1fdf
syncleon <oblapleon@gmail.com>
2026-02-25 15:31:44 +0200

    RedisURI.builder()

--- bidapi/src/main/kotlin/com/oblapleon/bidapi/common/config/Bucket4jConfig.kt
+++ bidapi/src/main/kotlin/com/oblapleon/bidapi/common/config/Bucket4jConfig.kt
@@ -3,6 +3,7 @@ package com.oblapleon.bidapi.common.config
 import io.github.bucket4j.distributed.proxy.ProxyManager
 import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager
 import io.lettuce.core.RedisClient
+import io.lettuce.core.RedisURI
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -10,12 +11,25 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class Bucket4jConfig(
     @Value("\${spring.data.redis.host:localhost}") private val redisHost: String,
-    @Value("\${spring.data.redis.port:6379}") private val redisPort: Int
+    @Value("\${spring.data.redis.port:6379}") private val redisPort: Int,
+    @Value("\${spring.data.redis.password:}") private val redisPassword: String
 ) {
 
     @Bean
     fun redisClient(): RedisClient {
-        return RedisClient.create("redis://$redisHost:$redisPort")
+        val builder = RedisURI.builder()
+            .withHost(redisHost)
+            .withPort(redisPort)
+
+        if (redisPassword.isNotBlank()) {
+            builder.withPassword(redisPassword.toCharArray())
+        }
+
+        if (redisHost.contains("upstash")) {
+            builder.withSsl(true)
+        }
+
+        return RedisClient.create(builder.build())
     }
 
     @Bean
